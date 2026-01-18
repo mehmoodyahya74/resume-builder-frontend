43205f6b5dce7471a946d809dc3cc6c7aa0385b5
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-01-17 22:13:53 -0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { useEffect } from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const GA_MEASUREMENT_ID = "G-VMZC20JG32"; // your GA ID
+
+const Root = () => {
+  useEffect(() => {
+    // Load GA script
+    const script1 = document.createElement("script");
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${GA_MEASUREMENT_ID}');
+    `;
+    document.head.appendChild(script2);
+  }, []);
+
+  return <App />;
+};
+
+createRoot(document.getElementById("root")!).render(<Root />);
+
