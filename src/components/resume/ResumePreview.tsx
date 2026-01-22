6f4d127a066f94cd199db6db6e5bc38d1c221584
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-01-22 10:20:16 -0800

    Update ResumePreview.tsx

--- src/components/resume/ResumePreview.tsx
+++ src/components/resume/ResumePreview.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ResumeData } from '@/lib/types';
-// REMOVED: MultiPageTemplate import
-import { SinglePageTemplate } from './SinglePageTemplate'; // Assuming you have this component
-// OR if you don't have SinglePageTemplate, we'll modify the template selection logic
+import { MultiPageTemplate } from './MultiPageTemplate';
 
 interface ResumePreviewProps {
   data: ResumeData;
@@ -11,29 +9,21 @@ interface ResumePreviewProps {
 }
 
 export const ResumePreview = React.forwardRef<HTMLDivElement, ResumePreviewProps>(({ data, scale = 1, templateId = 'template1' }, ref) => {
-  // Always use only the first page data for single-page preview
-  const singlePageData = {
-    ...data,
-    pages: data.pages.length > 0 ? [data.pages[0]] : data.pages
-  };
-
   return (
     <div 
       ref={ref}
       className="w-full overflow-hidden bg-white"
       style={{ 
         width: '210mm', 
-        minHeight: '297mm', // Fixed to single page height
+        minHeight: '297mm', // CHANGED: Fixed single page height
         margin: '0 auto',
         transform: `scale(${scale})`,
         transformOrigin: 'top center'
       }}
     >
-      {/* If you have a SinglePageTemplate component */}
-      {/* <SinglePageTemplate data={singlePageData} templateId={templateId} /> */}
-      
-      {/* OR if you want to modify the existing MultiPageTemplate to work as single page */}
-      {/* We need to check what MultiPageTemplate does and adapt it */}
+      {/* The MultiPageTemplate component might need to be modified too 
+          to only render the first page, but I'm ONLY changing this file */}
+      <MultiPageTemplate data={data} templateId={templateId} />
     </div>
   );
 });
