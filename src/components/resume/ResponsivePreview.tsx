454dee6e018023a2ef6dcd6ff3ea880cea5ae0c9
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-01-22 23:24:57 -0800

    Update ResponsivePreview.tsx

--- src/components/resume/ResponsivePreview.tsx
+++ src/components/resume/ResponsivePreview.tsx
@@ -37,7 +37,7 @@ export function ResponsivePreview({ children, scale = 1 }: ResponsivePreviewProp
     <div
       ref={containerRef}
       style={{
-        width: '80%',
+        width: '140%',
         height: '100%',
         overflow: 'hidden',
         display: 'flex',
