c67065fdfe68643f090c21f6c826db9a8330b4ca
mehmoodyahya74 <mehmoodyahya74@gmail.com>
2026-02-08 00:30:36 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, useLocation } from "wouter";
+update in this without changing any other thing      import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
